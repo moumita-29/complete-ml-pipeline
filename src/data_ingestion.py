ffb6dd9f5df5eb4e3bf96e1cab985e78d423e47e
Moumita <paulmoumita192@gmail.com>
2026-03-06 22:37:16 +0530

    params configured for the pipeline

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -2,6 +2,7 @@ import pandas as pd
 import os
 from sklearn.model_selection import train_test_split
 import logging
+import yaml
 
 
 # Create logs directory safely
@@ -29,6 +30,24 @@ file_handler.setFormatter(formatter)
 if not logger.handlers:
     logger.addHandler(console_handler)
     logger.addHandler(file_handler)
+
+
+def load_params(params_path: str) -> dict:
+    """Load parameters from a YAML file."""
+    try:
+        with open(params_path, 'r') as file:
+            params = yaml.safe_load(file)
+        logger.debug('Parameters retrieved from %s', params_path)
+        return params
+    except FileNotFoundError:
+        logger.error('File not found: %s', params_path)
+        raise
+    except yaml.YAMLError as e:
+        logger.error('YAML error: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error: %s', e)
+        raise
 
 
 def load_data(data_url: str) -> pd.DataFrame:
@@ -77,8 +96,9 @@ def save_data(train_data: pd.DataFrame, test_data: pd.DataFrame, data_path: str)
 
 def main():
     try:
-        test_size = 0.2
-
+        #test_size = 0.2
+        params = load_params(params_path='params.yaml')
+        test_size = params['data_ingestion']['test_size']
         data_url = "https://raw.githubusercontent.com/vikashishere/Datasets/refs/heads/main/spam.csv"
 
         df = load_data(data_url=data_url)
